fe1f2a4ee853656925cc223fd9c7f1c5d7546281
Yang Feng <fy413256@gmail.com>
2019-11-15 18:27:46 +0800

    add test

--- src/test/java/training/ParkingLotTest.java
+++ src/test/java/training/ParkingLotTest.java
@@ -3,6 +3,7 @@ package training;
 import model.Car;
 import model.ParkingLot;
 import model.Ticket;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -12,21 +13,39 @@ public class ParkingLotTest {
     public ExpectedException expectedException= ExpectedException.none();
 
     @Test
-    public void givenParkingLotIsNotFullWhenParkCarThenProvideAssociatedTicket() throws Exception {
+    public void givenParkingLotIsNotFullWhenParkCarThenProvideTicket() throws Exception {
         Car car = new Car("123");
         ParkingLot parkingLot = new ParkingLot(1);
         Ticket ticket = parkingLot.park(car);
         assert ticket != null;
+    }
+
+    @Test
+    public void givenParkingLotIsNotFullWhenParkCarThenProvideAssociatedTicket() throws Exception {
+        Car car = new Car("123");
+        ParkingLot parkingLot = new ParkingLot(1);
+        Ticket ticket = parkingLot.park(car);
         assert ticket.getCarNumber().equals(car.getCarNumber());
     }
 
     @Test
     public void givenParkingLotIsFullWhenParkCarThenFailedToParkAndRemindCarParkIsFull() throws Exception {
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot(0);
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.park(car);
         expectedException.expect(Exception.class);
         expectedException.expectMessage("car park is full");
         parkingLot.park(car);
+    }
+
+    @Test
+    public void givenParkingLotIsNotFullWhenParkMultipleCarsThenProvideTickets() throws Exception {
+        Car car = new Car("123");
+        ParkingLot parkingLot = new ParkingLot(2);
+        Ticket ticket1 = parkingLot.park(car);
+        Ticket ticket2 = parkingLot.park(car);
+        assert ticket1!=null;
+        assert ticket2!=null;
     }
 
     @Test
